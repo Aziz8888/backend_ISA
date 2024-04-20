e66c1a83159afd294ecce79ae89edce0bed5c18c
23karim <karim.kekli@esprit.tn>
2024-04-20 14:12:55 +0200

    claudinary

---
+++ middlewares/cloudinary.js
@@ -0,0 +1,9 @@
+
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dwkqcg27p', 
+  api_key: '284479431481554', 
+  api_secret: 'oq0Ulj0W1r3M6NftFBoqKMcG3cI' 
+});
+export default cloudinary;
