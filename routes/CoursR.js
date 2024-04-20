257b150f40094c686e3511ee85576c0b8b514a8f
abir <abir.belkaroui@gmail.com>
2024-04-20 15:21:54 +0200

    integration with karim

--- routes/CoursR.js
+++ routes/CoursR.js
@@ -1,13 +1,10 @@
 import multer from "../middlewares/multer-config.js";
-import {
-  AjouterCoursR,
-  GetAllCours,
-  deleteById,
-} from "../controllers/CoursR.js";
+import { AjouterCoursR } from "../controllers/CoursR.js";
 import express from "express";
 const router = express.Router();
 
-router.route("/rec").post(multer, AjouterCoursR).get(GetAllCours);
-router.delete("/recid/:id", deleteById);
+router.route("/rec").post(multer, AjouterCoursR);
+//.get(GetAllCours);
+//router.delete("/recid/:id", deleteById);
 
 export default router;
