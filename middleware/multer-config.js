e155866571f14cb873d461a743034a4c4c5f11d8
abir <abir.belkaroui@gmail.com>
2024-04-20 14:45:21 +0200

    update file multer

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -3,37 +3,37 @@ import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
 const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-  "video/mp4": "mp4",
-  "application/pdf": "pdf",
+   "image/jpg": "jpg",
+   "image/jpeg": "jpg",
+   "image/png": "png",
+   "video/mp4": "mp4",
+   "application/pdf": "pdf",
 };
 
 const storage = diskStorage({
-  destination: (req, file, callback) => {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    callback(null, join(__dirname, "../public"));
-  },
-  filename: (req, file, callback) => {
-    const nameWithoutSpaces = file.originalname.split(" ").join("_");
-    const [name, originalExtension] = nameWithoutSpaces.split(".");
-    const extension = MIME_TYPES[file.mimetype];
-    const finalExtension = originalExtension || extension;
-    const filename = `${name}.${finalExtension}`;
-    callback(null, filename);
-  },
+   destination: (req, file, callback) => {
+      const __dirname = dirname(fileURLToPath(import.meta.url));
+      callback(null, join(__dirname, "../public"));
+   },
+   filename: (req, file, callback) => {
+      const nameWithoutSpaces = file.originalname.split(" ").join("_");
+      const [name, originalExtension] = nameWithoutSpaces.split('.');
+      const extension = MIME_TYPES[file.mimetype];
+      const finalExtension = originalExtension || extension;
+      const filename = `${name}.${finalExtension}`;
+      callback(null, filename);
+   },
 });
 
 export default multer({
-  storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 },
-  fileFilter: (req, file, callback) => {
-    const isValid = !!MIME_TYPES[file.mimetype];
-    if (isValid) {
-      callback(null, true);
-    } else {
-      callback(new Error("Invalid file type. Only PDF files are allowed."));
-    }
-  },
-}).single("source");
+    storage: storage,
+    limits: { fileSize: 50 * 1024 * 1024 },
+    fileFilter: (req, file, callback) => {
+       const isValid = !!MIME_TYPES[file.mimetype];
+       if (isValid) {
+          callback(null, true);
+       } else {
+          callback(new Error('Invalid file type. Only PDF files are allowed.'));
+       }
+    },
+ }).single("source");
