0f8e3dee008b55b042498e9e7950582e4216e4ad
abir <abir.belkaroui@gmail.com>
2024-04-20 14:31:25 +0200

    update file server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import compilateur from "./routes/compilateur.js";
+import testblanc from "./routes/testblanc.js";
 const app = express();
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 9090;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 9090;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/compilateur", compilateur);
+app.use("/testblanc", testblanc);
 
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
