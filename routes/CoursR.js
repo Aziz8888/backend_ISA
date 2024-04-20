59968bef9097e18faec4a727fd02d0a9976f6d76
abir <abir.belkaroui@gmail.com>
2024-04-20 14:49:54 +0200

    add route CoursR de karim pour integration

---
+++ routes/CoursR.js
@@ -0,0 +1,13 @@
+import multer from "../middlewares/multer-config.js";
+import {
+  AjouterCoursR,
+  GetAllCours,
+  deleteById,
+} from "../controllers/CoursR.js";
+import express from "express";
+const router = express.Router();
+
+router.route("/rec").post(multer, AjouterCoursR).get(GetAllCours);
+router.delete("/recid/:id", deleteById);
+
+export default router;
