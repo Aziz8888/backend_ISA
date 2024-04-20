7bfdab343a2ec7a43c610de9ca69ad809c2d5281
23karim <karim.kekli@esprit.tn>
2024-04-20 15:49:28 +0200

    integration avec abir comp

--- routes/compilateur.js
+++ routes/compilateur.js
@@ -1,8 +1,7 @@
-
-import {getQuiz} from "../controllers/compilateur.js";
+import { addCompilateur } from "../controllers/compilateur.js";
 import express from "express";
 const router = express.Router();
 
-router.route("/run-code").get(getQuiz);
+router.route("/run-code").post(addCompilateur);
 
 export default router;
