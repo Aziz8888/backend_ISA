91ef9ac1b414c1ab707af38a9968d024d48d5d97
23karim <karim.kekli@esprit.tn>
2024-04-20 15:30:33 +0200

    route Get

--- routes/CoursR.js
+++ routes/CoursR.js
@@ -1,9 +1,13 @@
 import multer from "../middlewares/multer-config.js";
-import { AjouterCoursR } from "../controllers/CoursR.js";
+import { AjouterCoursR, GetAllCours } from "../controllers/CoursR.js";
 import express from "express";
 const router = express.Router();
 
-router.route("/").post(multer,AjouterCoursR);
+router
+.route("/rec")
+.post(multer,AjouterCoursR)
+.get(GetAllCours);
+
 
 
 export default router;
