67205933173bb1ede5efc1adeefc69323018257f
abir <abir.belkaroui@gmail.com>
2024-04-23 23:02:09 +0200

    integration note abir+aziz

--- controllers/note.js
+++ controllers/note.js
@@ -188,60 +188,26 @@ export async function recommanderVideosEtudiant(req, res) {
     res.status(500).json({ message: "Internal server error" });
   }
 }
-/*
-import Test from '../models/test.js';
-import Note from '../models/note.js';
 
-// Cette fonction pourrait être améliorée pour gérer les erreurs et cas particuliers plus spécifiques
-export async function submitAnswers(req, res) {
+export async function getScoreByChapter(req, res) {
   try {
-    const { testId, answers } = req.body; // Récupération des données de la requête
-    const test = await Test.findById(testId).populate('questions'); // Trouver le test et peupler les questions
-    const studentId = req.user._id;
-    if (!test) {
-      return res.status(404).json({ message: 'Test not found' });
-    }
-
-    let totalScore = 0;
-    const notes = [];
-
-    // Itérer sur chaque question pour vérifier la réponse
-    test.questions.forEach(async (question, index) => {
-      const studentResponse = answers[index];
-      let score = 0;
-
-      if (question.type === 'Quiz') {
-        // Pour les Quiz, vérification exacte de la réponse
-        if (question.response.toLowerCase() === studentResponse.toLowerCase()) {
-          score = question.marks;
-        }
-      } else if (question.type === 'QA') {
-        // Pour les questions QA, vérification si les mots-clés sont inclus dans la réponse de l'étudiant
-        const responseKeywords = question.response.toLowerCase().split(" ");
-        const studentResponseWords = studentResponse.toLowerCase().split(" ");
-        const matchedWords = responseKeywords.filter(word => studentResponseWords.includes(word));
-        score = (matchedWords.length / responseKeywords.length) * question.marks;
-      }
-
-      // Créer et sauvegarder une note pour chaque réponse
-      const note = new Note({
-        studentId: mongoose.Types.ObjectId(studentId),
-        questionId: question._id,
-        testId: testId,
-        score,
-        chapitre: question.chapitre,
-        complexity: question.complexity
+      const userId = req.params.userId;
+      const notes = await Note.find({ studentId: userId, totalScoreByChapter: { $ne: null } });
+  
+      let combinedScoresByChapter = {};
+  
+      notes.forEach(note => {
+        Object.entries(note.totalScoreByChapter).forEach(([chapter, score]) => {
+          if (combinedScoresByChapter[chapter]) {
+            combinedScoresByChapter[chapter] += score;
+          } else {
+            combinedScoresByChapter[chapter] = score;
+          }
+        });
       });
-      
-      await note.save();
-      
-      // Ajoutez la note nouvellement créée à votre tableau de notes ou à toute autre logique de suivi.
-      notes.push({ questionId: question._id, score, chapitre: question.chapitre, complexity: question.complexity });
-      totalScore += score;
-    });
-
-    res.status(200).json({ studentId, testId, totalScore, notes });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-}*/
+  
+      res.json(combinedScoresByChapter);
+    } catch (error) {
+      res.status(500).send("Erreur lors de la récupération des scores par chapitre: " + error.message);
+    }
+}
--- routes/note.js
+++ routes/note.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import { submitAnswers,recommanderVideosEtudiant} from '../controllers/note.js'; // Assurez-vous que le chemin d'importation est correct
+import express from "express";
+import {
+  submitAnswers,
+  recommanderVideosEtudiant,getScoreByChapter
+} from "../controllers/note.js"; // Assurez-vous que le chemin d'importation est correct
 
 const router = express.Router();
 
 // Route pour soumettre les réponses d'un étudiant et calculer les notes
-router.post('/tests/submit/', submitAnswers);
-router.get('/rec/:testId/:studentId',recommanderVideosEtudiant);
-
+router.post("/tests/submit/", submitAnswers);
+router.get("/rec/:testId/:studentId", recommanderVideosEtudiant);
+router.get("/scores/:userId/", getScoreByChapter);
 
 export default router;
